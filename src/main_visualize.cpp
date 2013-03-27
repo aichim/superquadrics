0b7854bd43adbe8fbd8234aed07f0691fd3e8154
Alexandru Ichim <alex.e.ichim@gmail.com>
2013-03-27 14:15:03 -0700

    changeS

--- src/main_visualize.cpp
+++ src/main_visualize.cpp
@@ -9,40 +9,74 @@
 #include <vtkSmartPointer.h>
 #include <vtkSuperquadricSource.h>
 
-int main(int argc, char *argv[])
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+
+using namespace pcl;
+
+int
+main (int argc,
+      char **argv)
 {
-  // Create a superquadric
-  vtkSmartPointer<vtkSuperquadricSource> superquadricSource = vtkSmartPointer<vtkSuperquadricSource>::New();
-  superquadricSource->SetPhiRoundness(1.1);
-  superquadricSource->SetThetaRoundness(.2);
+  PCL_INFO ("Syntax: %s [-e1 x.x] [-e2 y.y] [-a z.z] [-b t.t] [-c u.u]\n", argv[0]);
 
-  // Create a mapper and actor
-  vtkSmartPointer<vtkPolyDataMapper> superquadricMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  superquadricMapper->SetInputConnection(superquadricSource->GetOutputPort());
+  /// Parse command line arguments
+  bool visualize_enabled = true;
+  console::parse_argument (argc, argv, "-visualize", visualize_enabled);
 
-  vtkSmartPointer<vtkActor> superquadricActor = vtkSmartPointer<vtkActor>::New();
-  superquadricActor->SetMapper(superquadricMapper);
+  double epsilon_1 = 0.;
+  console::parse_argument (argc, argv, "-e1", epsilon_1);
 
-  vtkSmartPointer<vtkRenderer> sceneRenderer =  vtkSmartPointer<vtkRenderer>::New();
+  double epsilon_2 = 0.;
+  console::parse_argument (argc, argv, "-e2", epsilon_2);
 
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  double a = 1.;
+  console::parse_argument (argc, argv, "-a", a);
 
-  renderWindow->AddRenderer(sceneRenderer);
+  double b = 1.;
+  console::parse_argument (argc, argv, "-b", b);
 
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
+  double c = 1.;
+  console::parse_argument (argc, argv, "-c", c);
 
-  // Add actors to the renderers
-  sceneRenderer->AddActor (superquadricActor);
+  PCL_INFO ("### Superquadric parameters:\n   epsilon_1: %f\n   epsilon_2: %f\n   a: %f\n   b: %f\n   c: %f\n",
+            epsilon_1, epsilon_2, a, b, c);
 
-  // Render once to figure out where the background camera will be
-  renderWindow->Render();
 
-  // Render again to set the correct view
-  renderWindow->Render();
 
-  // Interact with the window
-  renderWindowInteractor->Start();
+  if (visualize_enabled)
+  {
+    // Create a superquadric
+    vtkSmartPointer<vtkSuperquadricSource> superquadricSource = vtkSmartPointer<vtkSuperquadricSource>::New();
+    /// Piet Hein Super-Egg
+    //  superquadricSource->SetThetaRoundness (1.);
+    //  superquadricSource->SetPhiRoundness (0.8);
+    //  superquadricSource->SetScale (3., 3., 4.);
+
+    superquadricSource->SetThetaRoundness (epsilon_1); /// epsilon_1
+    superquadricSource->SetPhiRoundness (epsilon_2); /// epsilon_2
+    superquadricSource->SetScale (a, b, c);
+
+    // Create a mapper and actor
+    vtkSmartPointer<vtkPolyDataMapper> superquadricMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    superquadricMapper->SetInputConnection(superquadricSource->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> superquadricActor = vtkSmartPointer<vtkActor>::New();
+    superquadricActor->SetMapper(superquadricMapper);
+
+    vtkSmartPointer<vtkRenderer> sceneRenderer =  vtkSmartPointer<vtkRenderer>::New();
+
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+
+    renderWindow->AddRenderer(sceneRenderer);
+
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    sceneRenderer->AddActor (superquadricActor);
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+  }
 
   return EXIT_SUCCESS;
 }
