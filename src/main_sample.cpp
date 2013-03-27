20757836862f94880503e5377730f03ea14837c6
Alexandru Ichim <alex.e.ichim@gmail.com>
2013-03-27 16:27:36 -0700

    can now sample superquadric function with VTK and save it as PCD

--- src/main_sample.cpp
+++ src/main_sample.cpp
@@ -8,6 +8,12 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkSmartPointer.h>
 #include <vtkSuperquadricSource.h>
+#include <vtkSuperquadric.h>
+#include <vtkSampleFunction.h>
+#include <vtkImageData.h>
+#include <vtkContourFilter.h>
+
+#include <pcl/io/pcd_io.h>
 
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
@@ -18,7 +24,7 @@ int
 main (int argc,
       char **argv)
 {
-  PCL_INFO ("Syntax: %s [-e1 x.x] [-e2 y.y] [-a z.z] [-b t.t] [-c u.u]\n", argv[0]);
+  PCL_INFO ("Syntax: %s [-e1 x.x] [-e2 y.y] [-a z.z] [-b t.t] [-c u.u] [-visualize 0/1] [-output xxx.pcd]\n", argv[0]);
 
   /// Parse command line arguments
   bool visualize_enabled = true;
@@ -39,11 +45,53 @@ main (int argc,
   double c = 1.;
   console::parse_argument (argc, argv, "-c", c);
 
+  std::string output_file = "";
+  console::parse_argument (argc, argv, "-output", output_file);
+
   PCL_INFO ("### Superquadric parameters:\n   epsilon_1: %f\n   epsilon_2: %f\n   a: %f\n   b: %f\n   c: %f\n",
             epsilon_1, epsilon_2, a, b, c);
 
 
+  /// Sample the superquadric to a pcd file
+  if (output_file != "")
+  {
+    vtkSmartPointer<vtkSuperquadric> superquadric = vtkSmartPointer<vtkSuperquadric>::New ();
+    superquadric->SetThetaRoundness (epsilon_1); /// epsilon_1
+    superquadric->SetPhiRoundness (epsilon_2); /// epsilon_2
+    superquadric->SetScale (a, b, c);
 
+    vtkSmartPointer<vtkSampleFunction> sample_function = vtkSmartPointer<vtkSampleFunction>::New ();
+    sample_function->SetSampleDimensions (200, 200, 200);
+    sample_function->SetImplicitFunction (superquadric);
+    sample_function->SetModelBounds (-1, 1, -1, 1, -1, 1);
+
+
+    vtkSmartPointer<vtkContourFilter> sample_contour = vtkSmartPointer<vtkContourFilter>::New ();
+    sample_contour->SetInputConnection (sample_function->GetOutputPort ());
+    sample_contour->SetValue (0, 0.0);
+    sample_contour->Update ();
+    vtkPolyData *poly_data = sample_contour->GetOutput ();
+
+    PCL_INFO ("poly data cells %ld, points %ld\n",
+              poly_data->GetNumberOfCells (), poly_data->GetNumberOfPoints ());
+
+
+    /// Convert poly data to PCL format
+    PointCloud<PointXYZ> cloud;
+    cloud.resize (poly_data->GetNumberOfPoints ());
+    double xyz[3];
+    for (vtkIdType p_i = 0; p_i < poly_data->GetNumberOfPoints (); ++p_i)
+    {
+      poly_data->GetPoint (p_i, &xyz[0]);
+      cloud[p_i].x = xyz[0];
+      cloud[p_i].y = xyz[1];
+      cloud[p_i].z = xyz[2];
+    }
+    io::savePCDFile (output_file, cloud, true);
+  }
+
+
+  /// Visualize the super quadric
   if (visualize_enabled)
   {
     // Create a superquadric
@@ -56,6 +104,8 @@ main (int argc,
     superquadricSource->SetThetaRoundness (epsilon_1); /// epsilon_1
     superquadricSource->SetPhiRoundness (epsilon_2); /// epsilon_2
     superquadricSource->SetScale (a, b, c);
+    superquadricSource->SetPhiResolution (1000);
+    superquadricSource->SetThetaResolution (1000);
 
     // Create a mapper and actor
     vtkSmartPointer<vtkPolyDataMapper> superquadricMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
